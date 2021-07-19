37b878fe41d15f9701c50ac10a8eb2216c9e4670
CODESQUAREL <86656524+CODESQUAREL@users.noreply.github.com>
2021-07-19 12:18:21 +0900

    "7/19 my_page 회원탈퇴 기능 구현

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
-from accountapp.views import code_square, AccountCreateView, AccountDetailView, AccountUpdateView
+from accountapp.views import code_square, AccountCreateView, AccountDetailView, AccountUpdateView, AccountDeleteView
 
 app_name = "accountapp"
 
@@ -17,5 +17,7 @@ urlpatterns = [
 
     path('detail/<int:pk>', AccountDetailView.as_view(), name = 'detail'),
 
-    path('update/<int:pk>', AccountUpdateView.as_view(), name = 'update')
+    path('update/<int:pk>', AccountUpdateView.as_view(), name = 'update'),
+
+    path('delete/<int:pk>', AccountDeleteView.as_view(), name = 'delete')
 ]
