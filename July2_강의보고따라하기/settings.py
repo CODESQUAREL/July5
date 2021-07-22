6f22be7873fd2fcb06138b5ef0d1708e59478a11
CODESQUAREL <86656524+CODESQUAREL@users.noreply.github.com>
2021-07-22 12:43:56 +0900

    7/22 profileapp 생성, url 지정

--- July2_강의보고따라하기/settings.py
+++ July2_강의보고따라하기/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'bootstrap4',
     'accountapp',
+    'profileapp'
 ]
 
 MIDDLEWARE = [
--- July2_강의보고따라하기/urls.py
+++ July2_강의보고따라하기/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('account/', include('accountapp.urls'))
+    path('account/', include('accountapp.urls')),
+    path('profiles/', include('profileapp.urls'))
 ]
