4add467eaf2b9491d3bbd617737676b809ff657a
CODESQUAREL <86656524+CODESQUAREL@users.noreply.github.com>
2021-07-22 11:55:06 +0900

    7/22 Decorator 함수 추가

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.http import HttpResponse, HttpResponseRedirect, HttpResponseForbidden
@@ -5,31 +6,31 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.urls import reverse, reverse_lazy
+from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
 
+from accountapp.decorator import account_ownership_required
 from accountapp.forms import AccountCreationForm
 from accountapp.models import CODE_SQUARE
 
-
+@login_required#(login_url:reverse_lazy('accountapp:login'))
 def code_square(request):
-    if request.user.is_authenticated:
-        if request.method == "POST":
+    if request.method == "POST":
 
-            temp = request.POST.get('input_text')
+        temp = request.POST.get('input_text')
 
-            new_code_square = CODE_SQUARE()
-            new_code_square.text = temp
-            new_code_square.save()
+        new_code_square = CODE_SQUARE()
+        new_code_square.text = temp
+        new_code_square.save()
 
-            code_square_list = CODE_SQUARE.objects.all()
+        # code_square_list = CODE_SQUARE.objects.all()
 
-            return HttpResponseRedirect(reverse('accountapp:CODE_SQUARE'))
-        else:
-            code_square_list = CODE_SQUARE.objects.all()
-            return render(request, 'accountapp/CODE_SQUARE.html',
-                          context = {'code_square_list': code_square_list})
+        return HttpResponseRedirect(reverse('accountapp:CODE_SQUARE'))
     else:
-        return HttpResponseRedirect(reverse('accountapp:login'))
+        code_square_list = CODE_SQUARE.objects.all()
+        return render(request, 'accountapp/CODE_SQUARE.html',
+                      context = {'code_square_list': code_square_list})
+
 
 
 class AccountCreateView(CreateView):
@@ -50,6 +51,11 @@ class AccountDetailView(DetailView):
 
 # 이거 작성하고 urls 에서 라우팅하는 것을 꼭 작성해줘야함
 
+has_ownership = [login_required, account_ownership_required]
+#method_decorator는 리스ㅌ 형태도 불러올 수 있다. (기능 두가지를 불러올 수 있는 것)
+
+@method_decorator(has_ownership, 'get')
+@method_decorator(has_ownership, 'post')
 class AccountUpdateView(UpdateView):
     model = User
     form_class = AccountCreationForm
@@ -57,35 +63,36 @@ class AccountUpdateView(UpdateView):
     success_url = reverse_lazy('accountapp:CODE_SQUARE')
     template_name = 'accountapp/update.html'
 
-    def get(self, request, *args, **kwargs):
-        if request.user.is_authenticated and self.get_object() == request.user: #self.get_object는 pk(target_user)로 이해한다.
-            return super().get(request, *args, **kwargs)
-        else:
-            return HttpResponseForbidden()
+    # def get(self, request, *args, **kwargs):
+    #     if request.user.is_authenticated and self.get_object() == request.user: #self.get_object는 pk(target_user)로 이해한다.
+    #         return super().get(request, *args, **kwargs)
+    #     else:
+    #         return HttpResponseForbidden()
+    #
+    # def post(self, request, *args, **kwargs):
+    #     if request.user.is_authenticated and self.get_object() == request.user:
+    #         return super().post(request, *args, **kwargs)
+    #     else:
+    #         return HttpResponseForbidden()
 
-    def post(self, request, *args, **kwargs):
-        if request.user.is_authenticated and self.get_object() == request.user:
-            return super().post(request, *args, **kwargs)
-        else:
-            return HttpResponseForbidden()
 
-
-
+@method_decorator(has_ownership, 'get')
+@method_decorator(has_ownership, 'post')
 class AccountDeleteView(DeleteView):
     model = User
     context_object_name = 'target_user'
     success_url = reverse_lazy('accountapp:CODE_SQUARE')
     template_name = 'accountapp/delete.html'
 
-    def get(self, request, *args, **kwargs):
-        if request.user.is_authenticated and self.get_object() == request.user:
-            return super().get(request, *args, **kwargs)
-        else:
-            return HttpResponseForbidden()
-
-    def post(self, request, *args, **kwargs):
-        if request.user.is_authenticated and self.get_object() == request.user:
-            return super().post(request, *args, **kwargs)
-        else:
-            return HttpResponseForbidden()
-
+    # def get(self, request, *args, **kwargs):
+    #     if request.user.is_authenticated and self.get_object() == request.user:
+    #         return super().get(request, *args, **kwargs)
+    #     else:
+    #         return HttpResponseForbidden()
+    #
+    # def post(self, request, *args, **kwargs):
+    #     if request.user.is_authenticated and self.get_object() == request.user:
+    #         return super().post(request, *args, **kwargs)
+    #     else:
+    #         return HttpResponseForbidden()
+    #
