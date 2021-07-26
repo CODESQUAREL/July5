23688434ded822c869e5bf77666d0a0b2391e3a3
CODESQUAREL <86656524+CODESQUAREL@users.noreply.github.com>
2021-07-26 12:24:52 +0900

    7/26 forms, create

--- profileapp/forms.py
+++ profileapp/forms.py
@@ -8,6 +8,6 @@ class ProfileCreationForm(ModelForm):
         #이미지라는 데이터는 RGB로 이루어진 픽셀데이터가 있고
         #메타데이터 : 이미지 외적인 요소(언제,어떤카메라,어디)를 말한다.
         model = Profile
-        fields = ['image', 'nickname', 'massege'] #이 세가지의 입력을 받을 것이다
+        fields = ['image', 'nickname', 'message'] #이 세가지의 입력을 받을 것이다
         #models.py에 만든 것은 4개인데 왜 입력은 3개만 받느냐?
-        #
+        #user는 넘겨받지않음
--- profileapp/views.py
+++ profileapp/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+# 로직 작성
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+
+from profileapp.forms import ProfileCreationForm
+from profileapp.models import Profile
+
+
+class ProfileCreateView(CreateView):
+    model = Profile
+    form_class = ProfileCreationForm
+    success_url = reverse_lazy('accountapp:CODE_SQUARE')
+    template_name = 'profileapp/create.html'
+
+
+    #------> urls.py
