4d0aea35ac16f412776782557f5322bfedbf9908
CODESQUAREL <86656524+CODESQUAREL@users.noreply.github.com>
2021-07-29 12:16:53 +0900

    7/29 bug fix

--- July2_강의보고따라하기/urls.py
+++ July2_강의보고따라하기/urls.py
@@ -20,6 +20,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('account/', include('accountapp.urls')),
+    path('accounts/', include('accountapp.urls')),
     path('profiles/', include('profileapp.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
