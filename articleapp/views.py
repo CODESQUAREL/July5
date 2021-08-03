5e7e4b4c0dea5ebec1f95a28e617089f99c5c63e
CODESQUAREL <86656524+CODESQUAREL@users.noreply.github.com>
2021-08-03 17:32:19 +0900

    8/3 articles/detail 생성 및 게시글 업로드 후 페이지 확인

--- articleapp/views.py
+++ articleapp/views.py
@@ -2,17 +2,27 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic import CreateView, DetailView
 
 from articleapp.forms import ArticleCreationForm
 from articleapp.models import Article
+
 
 class ArticleCreateView(CreateView):
     model = Article
     form_class = ArticleCreationForm
     success_url = reverse_lazy('articleapp:list')
     template_name = 'articleapp/create.html'
-    
+
     def form_valid(self, form):
-        form.instance.writer = self.request.user #request를 보내는 유저로 데이터베이스에 writer로 설정해준 것
+        form.instance.writer = self.request.user
         return super().form_valid(form)
+
+
+class ArticleDetailView(DetailView):
+    model = Article
+    context_object_name = 'target_article'
+    template_name = 'articleapp/detail.html'
+
+
+
