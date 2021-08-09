9c817093b44dc5c7b6c9bfbb77d947b0281384ec
CODESQUAREL <86656524+CODESQUAREL@users.noreply.github.com>
2021-08-09 12:43:02 +0900

    8/9 delete 설정

--- commentapp/urls.py
+++ commentapp/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from commentapp.views import CommentCreateView
+from commentapp.views import CommentCreateView, CommentDeleteView
 
 app_name = 'commentapp'
 
 urlpatterns = [
     path('create/', CommentCreateView.as_view(), name='create'),
+    path('delete/', CommentDeleteView.as_view(), name='delete')
 ]
