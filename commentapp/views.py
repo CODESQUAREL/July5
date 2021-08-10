3df8c6f9ca9d39874278ae98301d8a4ee47eb7ca
CODESQUAREL <86656524+CODESQUAREL@users.noreply.github.com>
2021-08-10 17:13:55 +0900

    8/10 comment 작성,삭제 시 로그인 확인하는 decorate 작성

--- commentapp/views.py
+++ commentapp/views.py
@@ -1,13 +1,17 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 # Create your views here.
 from django.urls import reverse
+from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, DeleteView
 
+from commentapp.decorators import comment_ownership_required
 from commentapp.forms import CommentCreationForm
 from commentapp.models import Comment
 
-
+@method_decorator(login_required, 'get')
+@method_decorator(login_required, 'post')
 class CommentCreateView(CreateView):
     model = Comment
     form_class = CommentCreationForm
@@ -21,7 +25,8 @@ class CommentCreateView(CreateView):
     def get_success_url(self):
         return reverse('articleapp:detail', kwargs={'pk': self.object.article.pk})
 
-
+@method_decorator(comment_ownership_required, 'get')
+@method_decorator(comment_ownership_required, 'post')
 class CommentDeleteView(DeleteView):
     model = Comment
     context_object_name = 'target_comment'
