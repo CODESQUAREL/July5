ef8919235637bae9d3b53dc14a8657ada1703aa1
CODESQUAREL <86656524+CODESQUAREL@users.noreply.github.com>
2021-08-17 16:44:23 +0900

    8/17 projects/list

--- projectapp/urls.py
+++ projectapp/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from projectapp.views import ProjectCreateView, ProjectDetailView
+from projectapp.views import ProjectCreateView, ProjectDetailView, ProjectListView
 
 app_name = 'projectapp'
 
@@ -8,4 +8,5 @@ app_name = 'projectapp'
 urlpatterns = [
     path('create/', ProjectCreateView.as_view(), name='create'),
     path('detail/<int:pk>', ProjectDetailView.as_view(), name='detail'),
+    path('list/', ProjectListView.as_view(), name='list'),
 ]
