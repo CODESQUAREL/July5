b076a5f2bf535dfd9b412898b037492ad8dbab71
CODESQUAREL <86656524+CODESQUAREL@users.noreply.github.com>
2021-08-24 18:30:45 +0900

    8/24 messages ERROR태그 -> danger(빨강)으로 고정 // like button design

--- July2_강의보고따라하기/settings.py
+++ July2_강의보고따라하기/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 from pathlib import Path
 
+from django.contrib.messages import constants as messages
 
 
 import environ
@@ -38,6 +39,9 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+}
 
 # Application definition
 
