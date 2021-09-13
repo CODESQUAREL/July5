21e10febe578b7b12200e13990a27c0a2624d3e7
CODESQUAREL <86656524+CODESQUAREL@users.noreply.github.com>
2021-09-13 12:46:52 +0900

    9/13 settings - local, deploy 분리

--- July2_강의보고따라하기/settings/base.py
+++ July2_강의보고따라하기/settings/base.py
@@ -18,26 +18,8 @@ from django.contrib.messages import constants as messages
 import environ
 from django.urls import reverse_lazy
 
-env = environ.Env(
-    # set casting, default value
-    DEBUG=(bool, False)
-)
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-# reading .env file
-environ.Env.read_env(
-    env_file=os.path.join(BASE_DIR, '.env')
-)
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = ["*"]
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 MESSAGE_TAGS = {
     messages.ERROR: 'danger',
@@ -92,17 +74,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'July2_강의보고따라하기.wsgi.application'
-
-
-# Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 
 # Password validation
--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'July2_강의보고따라하기.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'July2_강의보고따라하기.settings.local')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
